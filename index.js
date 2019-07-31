491c73b16ecd2667fbee0e94c75404e920d24e7d
Hamed Taheri <hamed.taheri32@gmail.com>
2019-07-31 19:21:07 +0430

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import { SliderBox } from './SliderBox';
+/**
+ * @format
+ */
 
-export { SliderBox };
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
