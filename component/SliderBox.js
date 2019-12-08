470c3932fad16da124ab5bac3d9a1d9a197236b1
FreakyCoder <kurayogun@gmail.com>
2019-12-08 14:24:24 +0300

    Carousel & Pagination & ImageComponent are customizable now. A bit code refactory & style is separated

--- component/SliderBox.js
+++ component/SliderBox.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
-import {
-  View,
-  Image,
-  TouchableHighlight,
-  Dimensions,
-  StyleSheet
-} from 'react-native';
+import React, { Component } from "react";
+import { View, Image, TouchableHighlight, Dimensions } from "react-native";
 
-import Carousel, { Pagination } from 'react-native-snap-carousel'; //Thank From distributer(s) of this lib
+import Carousel, { Pagination } from "react-native-snap-carousel"; //Thank From distributer(s) of this lib
+import styles from "./SliderBox.style";
 
 // -------------------Props---------------------
 // images;
@@ -22,7 +17,7 @@ import Carousel, { Pagination } from 'react-native-snap-carousel'; //Thank From 
 // ImageComponent;
 // paginationBoxStyle;
 
-const width = Dimensions.get('window').width;
+const width = Dimensions.get("window").width;
 export class SliderBox extends Component {
   constructor(props) {
     super(props);
@@ -49,92 +44,89 @@ export class SliderBox extends Component {
     }
   }
   _renderItem({ item, index }) {
-    const { ImageComponent } = this.props;
+    const { ImageComponent, sliderBoxHeight } = this.props;
     return (
       <TouchableHighlight
         key={index}
         onPress={() => this.onCurrentImagePressedHandler(index)}
       >
         <ImageComponent
-          style={{ width: null, height: this.props.sliderBoxHeight || 200 }}
+          style={{ width: null, height: sliderBoxHeight || 200 }}
           source={{ uri: item }}
+          {...this.props}
         />
       </TouchableHighlight>
     );
   }
   get pagination() {
-    const { paginationBoxStyle } = this.props;
+    const { currentImage } = this.state;
+    const {
+      images,
+      dotStyle,
+      dotColor,
+      inactiveDotColor,
+      paginationBoxStyle,
+      paginationBoxVerticalPadding
+    } = this.props;
     return (
       <Pagination
         borderRadius={2}
-        dotsLength={this.props.images.length}
-        activeDotIndex={this.state.currentImage}
+        dotsLength={images.length}
+        activeDotIndex={currentImage}
+        dotStyle={dotStyle || styles.dotStyle}
+        dotColor={dotColor || colors.dotColors}
+        inactiveDotColor={inactiveDotColor || colors.white}
+        inactiveDotScale={0.8}
+        carouselRef={this._ref}
+        inactiveDotOpacity={0.8}
+        tappableDots={!!this._ref}
         containerStyle={[
           styles.paginationBoxStyle,
-          this.props.paginationBoxVerticalPadding
-            ? { paddingVertical: this.props.paginationBoxVerticalPadding }
+          paginationBoxVerticalPadding
+            ? { paddingVertical: paginationBoxVerticalPadding }
             : {},
           paginationBoxStyle ? paginationBoxStyle : {}
         ]}
-        dotStyle={this.props.dotStyle || styles.dotStyle}
-        dotColor={this.props.dotColor || colors.dotColors}
-        inactiveDotColor={this.props.inactiveDotColor || colors.white}
-        inactiveDotOpacity={0.8}
-        inactiveDotScale={0.8}
-        carouselRef={this._ref}
-        tappableDots={!!this._ref}
+        {...this.props}
       />
     );
   }
   render() {
+    const {
+      images,
+      circleLoop,
+      autoplay,
+      parentWidth,
+      loopClonesPerSide
+    } = this.props;
     return (
       <View style={{ borderRadius: 2 }}>
         <Carousel
-          borderTopRightRadius={2}
+          layout={"default"}
           borderTopLeftRadius={2}
+          borderTopRightRadius={2}
+          data={images}
           ref={c => (this._ref = c)}
-          data={this.props.images}
+          loop={circleLoop || false}
+          autoplay={autoplay || false}
+          itemWidth={parentWidth || width}
+          sliderWidth={parentWidth || width}
+          loopClonesPerSide={loopClonesPerSide || 5}
           renderItem={item => this._renderItem(item)}
           onSnapToItem={index => this.setState({ currentImage: index })}
-          layout={'default'}
-          sliderWidth={this.props.parentWidth || width}
-          itemWidth={this.props.parentWidth || width}
-          loop={this.props.circleLoop || false}
-          autoplay={this.props.autoplay || false}
-          loopClonesPerSide={this.props.loopClonesPerSide || 5}
+          {...props}
         />
-        {this.props.images.length > 1 && this.pagination}
+        {images.length > 1 && this.pagination}
       </View>
     );
   }
 }
 
 const colors = {
-  dotColors: '#BDBDBD',
-  white: '#FFFFFF'
+  dotColors: "#BDBDBD",
+  white: "#FFFFFF"
 };
 
 SliderBox.defaultProps = {
   ImageComponent: Image
 };
-
-export const styles = StyleSheet.create({
-  paginationBoxStyle: {
-    position: 'absolute',
-    bottom: 0,
-    padding: 0,
-    alignItems: 'center',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    paddingVertical: 10
-  },
-  dotStyle: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    marginHorizontal: 0,
-    padding: 0,
-    margin: 0,
-    backgroundColor: 'rgba(128, 128, 128, 0.92)'
-  }
-});
